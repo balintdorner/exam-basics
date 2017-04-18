ec2fde8b604df41159c1f0f36dfff14c9a75d8fb
Balint Dorner <dorner.balint@gmail.com>
2017-04-18 09:52:29 +0200

    odd_avg unittest is prepared all the three checks work properly

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -2,13 +2,15 @@
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
 
-def odd_average(number_list):
-    counter = 0
-    sum_numbers = 0
-    average = 0
-    for number in number_list:
-        if number % 2 == 0:
-            counter += 1
-            sum_numbers += number
-            average = sum_numbers/counter
-    return average
+class OddAvg():
+    def odd_average(self, number_list):
+        self.number_list = number_list
+        counter = 0
+        sum_numbers = 0
+        average = 0
+        for number in self.number_list:
+            if number % 2 == 0:
+                counter += 1
+                sum_numbers += number
+                average = sum_numbers/counter
+        return average
--- oddavg/odd_avg_test.py
+++ oddavg/odd_avg_test.py
@@ -1,2 +1,22 @@
 import unittest
-from oddavg import *
+from odd_avg import *
+
+class TestOddAvg(unittest.TestCase):
+    def test_odd_average_empt_list(self, integer = []):
+        odd_avg = OddAvg()
+        integer = []
+        self.assertEqual(odd_avg.odd_average(integer), 0)
+
+    def test_odd_average_only_odd(self, integer = []):
+        odd_avg = OddAvg()
+        integer = [2, 6, 10]
+        self.assertEqual(odd_avg.odd_average(integer), 6)
+
+    def test_odd_average_integers(self, integer = []):
+        odd_avg = OddAvg()
+        integer = [1, 2, 3, 7, 12, 16]
+        self.assertEqual(odd_avg.odd_average(integer), 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
