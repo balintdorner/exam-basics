723327b7bf5475cb6f3b5c58e063951f3113e37f
Balint Dorner <dorner.balint@gmail.com>
2017-04-18 10:10:26 +0200

    exercise copy: class system is prepared, also the usage printing function

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,37 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+
+import sys
+
+class Control():
+
+    def __init__(self):
+        self.list_argv = []
+        self.arg_reader()
+        self.controller()
+
+    def arg_reader(self):
+        if len(sys.argv) <= 1:
+            self.list_argv = []
+        else:
+            self.list_argv = sys.argv[1:]
+
+    def controller(self):
+            if len(self.list_argv) == 0:
+                display.print_usage()
+
+
+class Model():
+    pass
+
+
+class Display():
+
+    def print_usage(self):
+        print(" Usage:\n copy [source] [destination] \n When just one argument is provided print out \n No destination provided \n When both arguments provided and the source is a file \n Read all contents from it and write it to the destination")
+
+
+model = Model()
+display = Display()
+control = Control()
