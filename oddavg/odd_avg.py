61eb5d5f7d15011852ff11135f777c50b74b7809
Balint Dorner <dorner.balint@gmail.com>
2017-04-18 09:29:17 +0200

    odd_avg function created, ready for unit testting

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,20 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average(number_list):
+    counter = 0
+    sum_numbers = 0
+    average = 0
+    for number in number_list:
+        if number % 2 == 0:
+            counter += 1
+            sum_numbers += number
+            average = sum_numbers/counter
+    return average
+
+
+
+
+
+print(odd_average([1,2,5,8,2,10]))
